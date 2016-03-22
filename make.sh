b0b94340b2caecadaf962fec102bad4803be888b
o <o@o1o.ch>
2016-03-22 16:52:18 +0100

    cleanup

--- make.sh
+++ make.sh
@@ -1,1 +1,1 @@
-clang++ --std=c++11 -g3 ir.cpp main.cpp && ./a.out | dot -Tps | ps2pdf  - a.pdf && evince a.pdf
+clang++ --std=c++11 -g3 ir.cpp main.cpp -o out/ir && out/ir | dot -Tps | ps2pdf  - out/ir.pdf && evince out/ir.pdf
